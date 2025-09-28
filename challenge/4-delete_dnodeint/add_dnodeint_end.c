fdc04dbc42f087ad628985ff23e93ec266da12f0
Shahad Fawaz <shahadfwaz2@gmail.com>
2025-09-28 23:52:39 +0300

    Update add_dnodeint_end.c

--- challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -1,37 +1,39 @@
+#include <string.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
  * add_dnodeint_end - Add a node at the end of a list
- * @head: Pointer to pointer to the first element of the list
- * @n: Number to store in the new element
  *
- * Return: Pointer to the new element, or NULL on failure
+ * @head: The address of the pointer to the first element of the list
+ * @n: The number to store in the new element
+ *
+ * Return: A pointer to the new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new = malloc(sizeof(dlistint_t));
-    dlistint_t *l;
+	dlistint_t *new;
+	dlistint_t *l;
 
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        new->prev = NULL;
-        *head = new;
-        return (new);
-    }
-
-    l = *head;
-    while (l->next != NULL)
-        l = l->next;
-
-    l->next = new;
-    new->prev = l;
-
-    return (new);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	l = *head;
+	while (l->next != NULL)
+	{
+		l = l->next;
+	}
+	l->next = new;
+	new->prev = l;
+	return (new);
 }
